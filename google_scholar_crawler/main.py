585be7273e8df0a3aa55e1b1a033cd9ceebb310f
ZitongWan <rostiute.monologue@gmail.com>
2025-05-08 10:11:51 +0800

    update github.io file

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -4,24 +4,48 @@ import json
 from datetime import datetime
 import os
 
-scholarly.set_timeout(30)
+time = 0
 
-author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+if time = 0:
+    time += 1
+    author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
 
-if author:
-    scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
-    name = author['name']
-    author['updated'] = str(datetime.now())
-    author['publications'] = {v['author_pub_id']: v for v in author['publications']}
-    print(json.dumps(author, indent=2))
-    os.makedirs('results', exist_ok=True)
-    with open(f'results/gs_data.json', 'w') as outfile:
-        json.dump(author, outfile, ensure_ascii=False)
+    if author:
+        scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+        name = author['name']
+        author['updated'] = str(datetime.now())
+        author['publications'] = {v['author_pub_id']: v for v in author['publications']}
+        print(json.dumps(author, indent=2))
+        os.makedirs('results', exist_ok=True)
+        with open(f'results/gs_data.json', 'w') as outfile:
+            json.dump(author, outfile, ensure_ascii=False)
 
-    shieldio_data = {
-        "schemaVersion": 1,
-        "label": "citations",
-        "message": f"{author['citedby']}",
-    }
-    with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
-        json.dump(shieldio_data, outfile, ensure_ascii=False)
+        shieldio_data = {
+            "schemaVersion": 1,
+            "label": "citations",
+            "message": f"{author['citedby']}",
+        }
+        with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
+            json.dump(shieldio_data, outfile, ensure_ascii=False)
+
+# scholarly.set_timeout(30)
+#
+# author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+#
+# if author:
+#     scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+#     name = author['name']
+#     author['updated'] = str(datetime.now())
+#     author['publications'] = {v['author_pub_id']: v for v in author['publications']}
+#     print(json.dumps(author, indent=2))
+#     os.makedirs('results', exist_ok=True)
+#     with open(f'results/gs_data.json', 'w') as outfile:
+#         json.dump(author, outfile, ensure_ascii=False)
+#
+#     shieldio_data = {
+#         "schemaVersion": 1,
+#         "label": "citations",
+#         "message": f"{author['citedby']}",
+#     }
+#     with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
+#         json.dump(shieldio_data, outfile, ensure_ascii=False)
